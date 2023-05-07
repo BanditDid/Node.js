a4af03b819d15c46dcfa626fbea5f10a06522592
BanditDid <banditonma@gmail.com>
2023-05-07 17:01:25 +0700

    v4

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const part = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static(part.join(__dirname,"/public/")))
+app.use(express.static(part.join(__dirname,"/docs/")))
 
 app.get("/",(req,res) => {
 
